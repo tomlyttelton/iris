6eef9441c4ea064501040d9d5bae556ce1b90754
Tom Lyttelton <tom.lyttelton@gmail.com>
2025-05-02 14:32:56 -0700

    Removing files that shouldn't be there

--- dist/generatePrompt.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generatePrompt = generatePrompt;
-async function generatePrompt(articles) {
-    return `Here's your tech news update: ${articles.join(', ')}.`;
-}
--- dist/getNewsData.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getNewsData = getNewsData;
-const apify_1 = require("apify");
-async function getNewsData() {
-    const run = await apify_1.Actor.call('apify/news-scraper', { category: 'technology' });
-    return ['No data']; //run.output?.articles?.slice(0, 3).map((a: any) => a.title) || ['No data'];
-}
