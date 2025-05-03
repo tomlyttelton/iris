04d9c5d67d0672621f3cfa981531ae038bee9425
Tom Lyttelton <tom.lyttelton@gmail.com>
2025-05-02 19:59:25 -0700

    Fix manual

--- src/manual.ts
+++ src/manual.ts
@@ -1,4 +1,7 @@
 import "dotenv/config";
+import { generateDigest } from "./generateDigest";
+import { getContext } from "./getContext";
+import { makeOutboundCall } from "./makeOutboundCall";
 
 async function main() {
   const phoneNumber = process.env.PHONE_NUMBER;
@@ -7,11 +10,11 @@ async function main() {
   if (!phoneNumber) {
     throw new Error("PHONE_NUMBER environment variable is required.");
   }
-  // const news = await getContext(query);
-  // console.log(news);
-  // const digest = await generateDigest(query, news);
-  // console.log(digest);
-  await makeOutboundCall(phoneNumber);
+  const news = await getContext(query);
+  console.log(news);
+  const digest = await generateDigest(query, news);
+  console.log(digest);
+  await makeOutboundCall(phoneNumber, digest);
 }
 
 main().catch(console.error);
